70c688cea9e9e3406937a5a51d5881e43d0ce5f4
siqiongbil <18955628948@163.com>
2026-09-16 11:35:20 +0800

    v1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.moread.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.4.0"
+        versionCode = 7
+        versionName = "1.4.1"
     }
 
     signingConfigs {
