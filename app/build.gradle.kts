15b20ad13c33efd283a75582c27a86b4af03217f
siqiongbil <18955628948@163.com>
2026-09-18 19:36:21 +0800

    v1.5.2: fix progress restore, update repo name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.moread.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.5.1"
+        versionCode = 10
+        versionName = "1.5.2"
     }
 
     signingConfigs {
